b67d02efdfa47da572f6c39e03ada6eee2191dc9
Michael Brissenden <34114234+Brisso@users.noreply.github.com>
2018-08-24 13:20:14 +1000

    Create Find a User's Security Identifier (SID).ps1

---
+++ Find a User's Security Identifier (SID).ps1
@@ -0,0 +1,9 @@
+## Brissotech
+## Script provided as is. Use at own risk. No guarantees or warranty provided.
+
+## Source - https://github.com/Brisso/PowerShell-Basics
+
+## Description
+## Script designed to show you all the User's Security Identifier (SID) on your computer.
+
+wmic useraccount get name,sid 
