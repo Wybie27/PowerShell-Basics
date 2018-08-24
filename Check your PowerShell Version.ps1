7609a46022156c898a591d56cd4be44c7b9c8d91
Michael Brissenden <34114234+Brisso@users.noreply.github.com>
2018-08-24 14:05:52 +1000

    Create Check your PowerShell Version.ps1

---
+++ Check your PowerShell Version.ps1
@@ -0,0 +1,9 @@
+## Brissotech
+## Script provided as is. Use at own risk. No guarantees or warranty provided.
+
+## Source - https://github.com/Brisso/PowerShell-Basics
+
+## Description
+## Script designed to show what version of Poweshell is on your computer.
+
+$PSVersionTable
